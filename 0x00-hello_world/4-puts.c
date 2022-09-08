ac914ab3f96c376f4dbe3bba0c4f0603ddadc3cc
wendybella <favekoka@gmail.com>
2022-09-08 16:35:45 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio .h>
+/**
+ * main - entry point of my code
+ * betty is awesome
+ * Return; 0 if completed successfully
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
