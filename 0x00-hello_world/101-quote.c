f11c760ed3a8d4e964e8a9a81fbe04a373493c3e
wendybella <favekoka@gmail.com>
2022-09-08 21:17:58 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistdio>
 /**
  * main-printing that plce is useful
  * with an error standard output
  * Return: 1 if properly executed
  */
-#include <unistd.h>
 int main(void)
 {
-write(4,"\"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",40;)
+write(2, "\"and that piece of art is useful" - Dora Korpar, 2015 -10 - 19 `\n", 58);
 return (1);
 }
