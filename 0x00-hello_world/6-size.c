e61c2f44169d9af86695d9822a056c15b39c3520
wendybella <favekoka@gmail.com>
2022-09-08 18:50:53 -0700

    too long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,12 @@ printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an double: %zu byte(s)\n", sizeof(int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
 printf("Size of a long: %zu byte(s)\n", sizeof(long));
+printf("Size of a int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a unsigned long int: %zu byte(s)\n", sizeof(unsigned long int));
+printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
+printf("Size of a short int: %zu byte(s)\n", sizeof(short int));
+printf("Size of a unsigned int: %zu byte(s)\n", sizeof(unsigned int));
 return (0);
 }
