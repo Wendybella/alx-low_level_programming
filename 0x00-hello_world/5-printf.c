746c5f540436d10d2e4bd391f20262117a91d245
wendybella <favekoka@gmail.com>
2022-09-08 17:58:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * betty please accept this offer
+ * return; 0 always after printf
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art"\n);
+return (0);
+}
