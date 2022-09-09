6d19dda725a639d6634deb30bb1df63596637539
wendybella <favekoka@gmail.com>
2022-09-09 10:23:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/** 
+ * main - show alphabet in lowercase
+ * print alphabet in lowercase
+ * Return: 0 normally
+ */
+int main(void)
+{
+char a = 97;
+while (a < 123)
+{
+putchar (a);
+a++;
+}
+putchar (10);
+return (0);
+}
