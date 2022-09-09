ac092848e6daf9fd0928a402074b906471c84c76
wendybella <favekoka@gmail.com>
2022-09-09 07:59:08 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,10 @@ int main(void)
 
 	int n;
 if (n == 0)
-printf("%d is zero\n", n);
+printf("zero\n", n);
 else if (n < 0)
-printf("%d is negative\n", n);
+printf("negative\n", n);
 else
-printf("%d is positive\n", n);
+printf("positive\n", n);
 return (0);
 }
