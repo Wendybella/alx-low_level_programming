d20f8feb766bd09f087b4f8878238a924128fed4
wendybella <favekoka@gmail.com>
2022-09-08 18:09:51 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main-entry point
  * betty please accept this offer
- * return; 0 always after printf
+ * Return: 0 always after printf
  */
 int main(void)
 {
