53f22eb604394adf902e052038b8211a12b3cbfc
wendybella <favekoka@gmail.com>
2022-09-09 10:01:41 -0700

    -print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - determines all single digit numbers from 0 in base 10
+ * single digits are to be printed
+ * Return: 0 always
+ */
+int main(void)
+{
+int x=28;
+while (x < 38)
+{
+putchar (x);
+x++
+}
+putchar (10);
+return (0);
+}
