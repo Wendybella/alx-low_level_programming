b5a88ad139351fd5bb81611390ebcb24a29ea45f
wendybella <favekoka@gmail.com>
2022-09-08 19:11:25 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-printf("Size of an ptr: %zu byte(s)\n", sizeof(ptr));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of a: off_t %zu byte(s)\n", sizeof(off_t));
 printf("Size of a long: %zu byte(s)\n", sizeof(long));
 printf("Size of a int: %zu byte(s)\n", sizeof(int));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of short: %zu byte(s)\n", sizeof(short));
 return (0);
 }
