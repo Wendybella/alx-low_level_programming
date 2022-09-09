f1b8e03cb70d09271edf373d2b2b6f9a063d9751
wendybella <favekoka@gmail.com>
2022-09-09 09:18:48 -0700

    5-print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - shows all single digit numbers of base 10 starting from 0
+ * print single digits numbers
+ * Return: always 0 success
+ */
+int main()
+{
+int n=0;
+while(n<10)
+{
+printf("n is: %d\n", n);
+n=n+1;
+}
+}
