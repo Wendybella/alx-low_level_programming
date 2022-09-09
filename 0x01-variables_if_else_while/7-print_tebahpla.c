5b1652bab887604f517a621ee591763b441e1221
wendybella <favekoka@gmail.com>
2022-09-09 14:58:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - reverse lowercase
+ * print reverse lowercase
+ * Return: 0 often
+ */
+int main(void)
+{
+int c = 122;
+while (c > 96)
+{
+putchar(c);
+c--;
+}
+putchar (10);
+return (0);
+}
