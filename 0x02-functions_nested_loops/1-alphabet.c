81854f08fe749b71e97375583040b5512354826f
wendybella <favekoka@gmail.com>
2022-09-14 15:44:49 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet
+ * in lowercase
+ * Return: 0 always successful
+ */
+void print_alphabet(void)
+{
+char n = 'a';
+char s = 'z';
+while (n <= s)
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
