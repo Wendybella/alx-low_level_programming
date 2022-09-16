05352663c759a53635956ea23c3114d4d55fc3d0
wendybella <favekoka@gmail.com>
2022-09-16 14:39:41 -0700

    int _isupper(int c)

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+ /**
+  * isupper.c - print uppercase letter
+  * return: 1(if c is uppercase) 0 (otherwise)
+  */
+int _isupper(int c);
+{
+int c;
+if(c >= 65 && c <= 90)
+{
+return(1);
+}
+else
+{
+return (0);
+}
+}
