624f7c7f3d0287547fb95ce5ab2a5b490e447b3d
wendybella <favekoka@gmail.com>
2022-09-28 15:07:10 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strlen_recursion - shows the length of a string
+ * @s: measured string
+ * Return: The length of the string
+ */
+
+int _strlen_recursion(char *s)
+
+{
+
+	int len = 0;
+
+	if (*s)
+
+	{
+
+		len++;
+
+		len += _strlen_recursion(s + 1);
+
+	}
+
+	return (len);
+}
