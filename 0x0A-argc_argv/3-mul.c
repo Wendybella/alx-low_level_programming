ba89329163d599147d42eea0a379df7c084cb0f8
wendybella <favekoka@gmail.com>
2022-09-30 15:14:38 -0700

    multiply x and y

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+/**
+ * main - Prints the multiplication of two numbers
+ * @argc: The number of arguments
+ * @argv: An array of arguments.
+ * Return: If the program receives two arguments - 0.
+ * if the program does not receive two arguments - 1.
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	int x, y, lens;
+
+	if (argc != 3)
+
+	{
+
+		printf("Error\n");
+
+		return (1);
+
+	}
+
+	x = atoi(argv[1]);
+
+	y = atoi(argv[2]);
+
+	lens = x * y;
+
+	printf("%d\n", lens);
+
+	return (0);
+
+}
