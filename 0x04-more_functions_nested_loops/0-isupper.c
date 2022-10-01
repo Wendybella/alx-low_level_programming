325fc179bbbc82845f3ba092aea7b83a0d46e76c
wendybella <favekoka@gmail.com>
2022-09-30 19:37:48 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,7 @@
   * _isupper.c - print uppercase letter
   * return: 1(if c is uppercase) 0 (otherwise)
   */
-int _isupper(int c);
+int _isupper(int c)
 {
-int uppercase;
-if (uppercase >= 65 && uppercase <= 90)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+return (c >= 'A' && c <= 'Z');
 }
